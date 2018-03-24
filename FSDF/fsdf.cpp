c07510f0c303061a110e6af98867323231ff34ae
szat <artabalt@gmail.com>
2018-03-23 22:28:32 -0400

    tree structure in header

--- FSDF/fsdf.cpp
+++ FSDF/fsdf.cpp
@@ -1,12 +1,55 @@
 #include "fsdf.h"
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Node
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-FSDF::FSDF()
-{
+OBBNode::OBBNode() {
+	this->left = nullptr;
+	this->right = nullptr;
+	this->nb_pts = 0;
+	this->depth = 0;
+	this->idx.clear();
 }
 
+void OBBNode::compute_obb() {
+	this->box.row(0) << 0, 0, 0;
+	this->box.row(1) << 0, 0, 0;
+	this->box.row(2) << 0, 0, 0;
+	this->box.row(3) << 0, 0, 0;
+	this->box.row(4) << 0, 0, 0;
+}
 
-FSDF::~FSDF()
-{
+OBBNode::Box OBBNode::get_obb() const {
+	return this->box;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Tree
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+OBBTree::OBBTree(MatrixXd pcl, MatrixXd normals) {
+	this->pcl = pcl; //I actually want a new copy of pcl
+	this->normals = normals;
+	this->max_depth = 4;
+	this->max_dist = 0;
+	this->nb_leaves = 0;
+	this->root = unique_ptr<OBBNode>(new OBBNode()); //owning
+}
+
+void OBBTree::build_tree() {
+	//one iteration
+	root->left = unique_ptr<OBBNode>(new OBBNode());
+	root->right = unique_ptr<OBBNode>(new OBBNode());
+	//stopping condition: max_level or obb statistics
+	return;
+}
+
+vector<int> OBBTree::ray_intersect(const Ref<const Vector3d> source, const Ref<const Vector3d> dir) const {
+	vector<int> pt_list;
+	return pt_list;
+}
+
+double OBBTree::query(const Ref<const Vector3d> pt) const {
+	return 0;
 }
--- FSDF/fsdf.h
+++ FSDF/fsdf.h
@@ -1,8 +1,60 @@
-#pragma once
-class FSDF
-{
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+
+using namespace std;
+using namespace Eigen;
+
+class OBBNode {
+typedef Matrix<double, 5, 3> Box;
+
+private:
+	vector<int> idx;
+	int nb_pts;
+	int depth;
+	Box box;
+	//box.row(0) = corner;
+	//box.row(1) = side1; 
+	//box.row(2) = side2; 
+	//box.row(3) = side3; 
+	//box.row(4) = evals; 
+
 public:
-	FSDF();
-	~FSDF();
+	unique_ptr<OBBNode> left;
+	unique_ptr<OBBNode> right;
+
+	// Special member functions, move semantic only
+	OBBNode();
+	OBBNode(const OBBNode& other) = delete;
+	OBBNode& operator=(const OBBNode& rhs) = delete;
+
+	void compute_obb(); 
+	Box get_obb() const;
 };
 
+class OBBTree {
+private:
+	// Members
+	int max_depth; //of the tree
+	int nb_leaves;
+	double max_dist; //between two vertices linked by an edge
+	MatrixXd pcl;
+	MatrixXd normals;
+	unique_ptr<OBBNode> root;
+
+	// Inner functions
+	vector<int> ray_intersect(const Ref<const Vector3d> source, const Ref<const Vector3d> dir) const;
+public:
+	// Special member functions, move semantic only
+	OBBTree(MatrixXd pcl, MatrixXd normals);
+	OBBTree(const OBBTree& other) = delete;
+	OBBTree& operator=(const OBBTree& rhs) = delete;
+
+	// User functions
+	void build_tree();	
+	double query(const Ref<const Vector3d> pt) const;
+};
