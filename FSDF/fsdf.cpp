1992bf152b39d78d0a1edb2401a25d93317adbdb
szat <artabalt@gmail.com>
2018-03-24 10:01:26 -0400

    changed Box type to MatrixXd

--- FSDF/fsdf.cpp
+++ FSDF/fsdf.cpp
@@ -4,7 +4,7 @@
 // Node
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-OBBNode::OBBNode() {
+OBBTree::OBBNode::OBBNode() {
 	this->left = nullptr;
 	this->right = nullptr;
 	this->nb_pts = 0;
@@ -12,15 +12,10 @@ OBBNode::OBBNode() {
 	this->idx.clear();
 }
 
-void OBBNode::compute_obb() {
-	this->box.row(0) << 0, 0, 0;
-	this->box.row(1) << 0, 0, 0;
-	this->box.row(2) << 0, 0, 0;
-	this->box.row(3) << 0, 0, 0;
-	this->box.row(4) << 0, 0, 0;
+void OBBTree::OBBNode::set_obb(MatrixXd box) {
 }
 
-OBBNode::Box OBBNode::get_obb() const {
+MatrixXd OBBTree::OBBNode::get_obb() const {
 	return this->box;
 };
 
@@ -44,6 +39,63 @@ void OBBTree::build_tree() {
 	//stopping condition: max_level or obb statistics
 	return;
 }
+
+MatrixXd OBBTree::compute_obb(vector<int> & idx) const {
+	MatrixXd box(5, 3);
+
+	// Slicing 
+	MatrixXd V(idx.size(), 3);
+	for (size_t i = 0; i < idx.size(); ++i) {
+		V.row(i) = this->pcl.row(idx.at(i));
+	}
+
+	Vector3d center = V.colwise().mean();
+	MatrixXd centered = V.rowwise() - center.transpose();
+	MatrixXd cov = (centered.adjoint() * centered) / double(V.rows() - 1);
+
+	// Get eigenvectors from point cloud
+	Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(cov);
+	Vector3d ev0 = eig.eigenvectors().col(0); //min
+	Vector3d ev1 = eig.eigenvectors().col(1); //mid
+	Vector3d ev2 = eig.eigenvectors().col(2); //max
+
+	// Vernier
+	Vector3d tMin = DBL_MAX * Vector3d::Ones(3);
+	Vector3d tMax = -DBL_MAX * Vector3d::Ones(3);
+
+	// Create oriented bounding box by projecting points onto eigenvectors.
+	for (size_t i = 0; i < centered.rows(); ++i) { //Still in col major, potential optimiation
+		Vector3d pt = centered.row(i);
+
+		// a + ab * dot(ap,ab)/dot(ab,ab), a = means, p = pt, b = amax (or amid or amin)
+		double t;
+		t = pt.dot(ev0) / ev0.norm();
+		if (t < tMin[0])
+			tMin[0] = t;
+		if (t > tMax[0])
+			tMax[0] = t;
+
+		t = pt.dot(ev1) / ev1.norm();
+		if (t < tMin[1])
+			tMin[1] = t;
+		if (t > tMax[1])
+			tMax[1] = t;
+
+		t = pt.dot(ev2) / ev2.norm();
+		if (t < tMin[2])
+			tMin[2] = t;
+		if (t > tMax[2])
+			tMax[2] = t;
+	}
+
+	box.row(0) = (tMax[0] - tMin[0]) * ev0; //side0
+	box.row(1) = (tMax[1] - tMin[1]) * ev1; //side1
+	box.row(2) = (tMax[2] - tMin[2]) * ev2; //side2
+	box.row(3) = center + tMin[0] * ev0 + tMin[1] * ev1 + tMin[2] * ev2; //corner
+	box.row(4) = eig.eigenvalues();
+
+	return box;
+};
 
 vector<int> OBBTree::ray_intersect(const Ref<const Vector3d> source, const Ref<const Vector3d> dir) const {
 	vector<int> pt_list;
--- FSDF/fsdf.h
+++ FSDF/fsdf.h
@@ -9,37 +9,35 @@
 using namespace std;
 using namespace Eigen;
 
-class OBBNode {
-typedef Matrix<double, 5, 3> Box;
-
-private:
-	// Members
-	vector<int> idx;
-	int nb_pts;
-	int depth;
-	Box box;
-	//box.row(0) = corner;
-	//box.row(1) = side1; 
-	//box.row(2) = side2; 
-	//box.row(3) = side3; 
-	//box.row(4) = evals; 
-
-public:
-	// Members
-	unique_ptr<OBBNode> left;
-	unique_ptr<OBBNode> right;
-
-	// Special member functions, move semantic only
-	OBBNode();
-	OBBNode(const OBBNode& other) = delete;
-	OBBNode& operator=(const OBBNode& rhs) = delete;
-
-	// User functions
-	void compute_obb(); 
-	Box get_obb() const;
-};
-
 class OBBTree {
+	class OBBNode {
+	private:
+		// Members
+		vector<int> idx;
+		int nb_pts;
+		int depth;
+		MatrixXd box; //5 x 3
+		//box.row(0) = corner;
+		//box.row(1) = side1; 
+		//box.row(2) = side2; 
+		//box.row(3) = side3; 
+		//box.row(4) = evals; 
+	public:
+		// Members
+		unique_ptr<OBBNode> left;
+		unique_ptr<OBBNode> right;
+
+		// Special member functions, move semantic only
+		OBBNode();
+		OBBNode(const OBBNode& other) = delete;
+		OBBNode& operator=(const OBBNode& rhs) = delete;
+
+		// Tree functions
+		void set_obb(MatrixXd box);
+		MatrixXd get_obb() const;
+	};
+	// End nested class
+
 private:
 	// Members
 	int max_depth; //of the tree
@@ -50,10 +48,11 @@ private:
 	unique_ptr<OBBNode> root;
 
 	// Inner functions
+	MatrixXd compute_obb(vector<int> & idx) const;
 	vector<int> ray_intersect(const Ref<const Vector3d> source, const Ref<const Vector3d> dir) const;
 public:
 	// Special member functions, move semantic only
-	OBBTree(MatrixXd pcl, MatrixXd normals);
+	OBBTree(MatrixXd pcl, MatrixXd normals); //Want copy of structures
 	OBBTree(const OBBTree& other) = delete;
 	OBBTree& operator=(const OBBTree& rhs) = delete;
 
